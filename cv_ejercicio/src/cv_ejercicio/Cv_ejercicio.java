2ccc114e6e015675bc4806bef106d3609889cb0f
Usuario <Usuario@Usuario-PC>
2022-06-09 15:22:43 -0500

    vercion 1

---
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cv_ejercicio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cv_ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("vercion 1");
+        System.out.println("vercion 2");
+    }
+    
+}
